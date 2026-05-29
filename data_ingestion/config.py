45a405dd93e82586fc47f1dfbd3f4733ffa6d41d
zuzia <zalewska.zuzannaa@gmail.com>
2026-05-29 09:38:25 +0200

    configuration loader with debug

---
+++ data_ingestion/config.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = BASE_DIR / '.env'
+
+load_dotenv(dotenv_path=env_path)
+
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+WATCHMODE_API_KEY = os.getenv("WATCHMODE_API_KEY")
+
+if not TMDB_API_KEY:
+    raise ValueError("Missing TMDB_API_KEY in .env file.")
+
+if not WATCHMODE_API_KEY:
+    raise ValueError("Missing WATCHMODE_API_KEY in .env file.")
+
+print(f"TMDB Key: {TMDB_API_KEY}")
+print(f"Watchmode Key: {WATCHMODE_API_KEY}")
